2936b60fc88cd17b57f3dca3ed48890cbd6ed0bf
Tobias Hornberger <tobias.hornberger@falsemirror.de>
2019-03-24 20:02:57 +0000

    Migrated coverage analysis from coveralls to codecov.

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,24 @@
-import typescript from 'rollup-plugin-typescript2'
-import pkg from './package.json'
+import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
-    input: 'src/index.ts',
+    external: [
+        ...Object.keys(pkg.dependencies || {}),
+    ],
+    input: "src/index.ts",
     output: [
         {
             file: pkg.main,
-            format: 'cjs',
+            format: "cjs",
         },
         {
             file: pkg.module,
-            format: 'es',
+            format: "es",
         },
-    ],
-    external: [
-        ...Object.keys(pkg.dependencies || {}),
-        ...Object.keys(pkg.peerDependencies || {}),
     ],
     plugins: [
         typescript({
-            typescript: require('typescript'),
+            typescript: require("typescript"),
         }),
     ],
-}
+};
